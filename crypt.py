733995a2d04404dbe713d592baac0cbe7d693627
Grant Lindberg <grantlindberg4@gmail.com>
2018-03-26 17:54:27 -0700

    Creating working encrypt function

--- crypt.py
+++ crypt.py
@@ -1,6 +1,17 @@
-import struct
+import random
 
 from exponent import power
+
+
+def create_ciphertexts(public_key, block, k):
+    c1 = power(public_key[1], k, public_key[0])
+    # print("(%d^%d %% %d) = %d" % (public_key[1], k, public_key[0], c1))
+    
+    c2 = (power(public_key[2], k, public_key[0])*(block % public_key[0])) % public_key[0]
+    # print("((%d^%d %% %d)*(%d %% %d)) %% %d = %d" % (public_key[2], k, public_key[0], block, public_key[0], public_key[0], c2))
+
+    return (c1, c2)
+
 
 def decrypt(ciphertexts, private_key):
     first = power(ciphertexts[0], private_key[0]-1-private_key[2], private_key[0])
@@ -8,3 +19,23 @@ def decrypt(ciphertexts, private_key):
     result = first*second % private_key[0]
 
     return result.to_bytes(4, 'big')
+
+
+def encrypt(plaintext, public_key):
+    ciphertexts = []
+    with open(plaintext, 'rb') as f:
+        while True:
+            block = f.read(4)
+            if not block:
+                break
+            # print(block)
+            block = int.from_bytes(block, 'big')
+
+            k = random.randint(0, public_key[0]-1)
+
+            results = create_ciphertexts(public_key, block, k)
+            # print(results)
+            ciphertexts.append(results[0])
+            ciphertexts.append(results[1])
+
+    return ciphertexts
--- key_gen.py
+++ key_gen.py
@@ -1,20 +1,7 @@
 from exponent import power
 
 
-def create_public_keys():
-    p = 11
-    g = 2
-    d = 3
+def create_public_keys(private_key):
+    e2 = power(private_key[1], private_key[2], private_key[0])
 
-    second = power(g, d, p)
-
-    return (g, second, p)
-
-
-def create_ciphertexts(public_keys, m, k):
-    c1 = power(public_keys[0], k, public_keys[2])
-    # print("(%d^%d %% %d) = %d" % (public_keys[1], k, public_keys[2], power(public_keys[1], k, public_keys[2])))
-    # print("((%d^%d %% %d)*(%d %% %d)) %% %d = " % (public_keys[1], k, public_keys[2], m, public_keys[2], public_keys[2]))
-    c2 = (power(public_keys[1], k, public_keys[2])*(m % public_keys[2])) % public_keys[2]
-
-    return (c1, c2)
+    return (private_key[0], private_key[1], e2)
